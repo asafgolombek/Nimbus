5a9e9328d84e5607ac6fa79a2615e8cf25c1721d
AsafGolombek <asafgolombek@gmail.com>
2026-04-19 20:53:52 +0300

    feat(ui): add dashboard store slice for metrics/connectors/audit

--- packages/ui/src/store/index.ts
+++ packages/ui/src/store/index.ts
@@ -1,14 +1,20 @@
 import { create } from "zustand";
 import { type ConnectionSlice, createConnectionSlice } from "./slices/connection";
+import { createDashboardSlice, type DashboardSlice } from "./slices/dashboard";
 import { createOnboardingSlice, type OnboardingSlice } from "./slices/onboarding";
 import { createQuickQuerySlice, type QuickQuerySlice } from "./slices/quickQuery";
 import { createTraySlice, type TraySlice } from "./slices/tray";
 
-export type NimbusStore = ConnectionSlice & TraySlice & QuickQuerySlice & OnboardingSlice;
+export type NimbusStore = ConnectionSlice &
+  TraySlice &
+  QuickQuerySlice &
+  OnboardingSlice &
+  DashboardSlice;
 
 export const useNimbusStore = create<NimbusStore>()((...a) => ({
   ...createConnectionSlice(...a),
   ...createTraySlice(...a),
   ...createQuickQuerySlice(...a),
   ...createOnboardingSlice(...a),
+  ...createDashboardSlice(...a),
 }));
